90da5c88015fdbaa0bdc196414279dbf2d94e71b
lydiali9 <648823000@qq.com>
2019-10-30 15:22:08 +0800

    css处理 css-loader style-loader less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,7 @@ let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devServer: { // 开发服务器的配置
-        port: 3000,
-        progress: true,
-        contentBase: './build',
-        compress: true
-    },
-    mode: "production", // 模式
+    mode: "development", // 模式
     entry: './src/index.js', // 入口
     output: {
         filename: "bundle.[hash:5].js", // 打包后的文件名
@@ -24,5 +18,45 @@ module.exports = {
             },
             hash: true
         })
-    ]
+    ],
+    module: { // 模块
+        rules: [ // 规则 css-loader 解析@import这种语法的
+            {
+                test: /\.css$/,
+                use: [
+                    // style-loader用于将css插入到head的body中
+                    // loader单一
+                    // 多个loader需要一个数组
+                    // loader是需要顺序的， 默认是从右向左执行
+                    {
+                        loader: "style-loader",
+                        // options: {
+                        //     insertAt: 'top' // 指定自定义的css标签插入到 webpack打包后的css文件的上面，提高优先级
+                        // }
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    // style-loader用于将css插入到head的body中
+                    // loader单一
+                    // 多个loader需要一个数组
+                    // loader是需要顺序的， 默认是从右向左执行
+                    {
+                        loader: "style-loader",
+                    },
+                    'css-loader', // @import 路径解析
+                    'less-loader' // 把less -> css
+                ]
+            }
+        ]
+    }
+    // devServer: { // 开发服务器的配置
+    //     port: 3000,
+    //     progress: true,
+    //     contentBase: './build',
+    //     compress: true
+    // }
 }
